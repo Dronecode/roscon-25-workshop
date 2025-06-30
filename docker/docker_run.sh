bfe18a81f07482fb0a219bde955226700b54c961
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2025-06-30 08:36:56 +0100

    fix: remove comment in docker_run.sh
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -4,7 +4,6 @@ docker run -it --privileged \
     -e DISPLAY=$DISPLAY \
     --runtime=nvidia \
     -v $(pwd):/home/ubuntu/roscon-25-workshop \
-# TO-DO put AppImage in docker
     -v $(pwd)/PX4_Autopilot_SITL-x86_64.AppImage:/home/ubuntu/PX4_Autopilot_SITL-x86_64.AppImage:ro \
     --name=gz \
     px4/roscon-25-workshop bash
