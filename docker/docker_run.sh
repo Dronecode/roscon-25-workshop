c31d7b9ac304172207596d50be0cdb1653cc4d31
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2025-07-02 23:17:29 +0100

    chore: remove `--privileged` and `nvidia` runtime
    downside: Appimage needs to be extracted
    
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,9 +1,8 @@
-docker run -it --privileged \
+docker run -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
     --rm \
     -e DISPLAY=$DISPLAY \
-    --runtime=nvidia \
     -v $(pwd)/px4_roscon_25:/home/ubuntu/roscon-25-workshop_ws/src/ \
     -v $(pwd)/PX4_Autopilot_SITL-x86_64.AppImage:/home/ubuntu/PX4_Autopilot_SITL-x86_64.AppImage:ro \
-    --name=gz \
+    --name=px4-roscon-25 \
     px4/roscon-25-workshop bash
