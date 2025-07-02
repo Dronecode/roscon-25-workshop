a1de2ccaa98d2f9e440f9ee61c27e354081bfb00
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2025-07-02 22:11:58 +0100

    chore: add empty ROS 2 package
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -3,7 +3,7 @@ docker run -it --privileged \
     --rm \
     -e DISPLAY=$DISPLAY \
     --runtime=nvidia \
-    -v $(pwd):/home/ubuntu/roscon-25-workshop \
+    -v $(pwd)/px4_roscon_25:/home/ubuntu/roscon-25-workshop_ws/src/ \
     -v $(pwd)/PX4_Autopilot_SITL-x86_64.AppImage:/home/ubuntu/PX4_Autopilot_SITL-x86_64.AppImage:ro \
     --name=gz \
     px4/roscon-25-workshop bash
