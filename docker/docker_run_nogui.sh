034bc12edda9768d214845988f7103d9d2a46d2a
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2025-08-09 13:29:19 +0100

    chore: docker_run_nogui.sh exposed MAVLink port for gcs connection
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- docker/docker_run_nogui.sh
+++ docker/docker_run_nogui.sh
@@ -5,4 +5,5 @@ docker run -it \
     --rm \
     -v ${SCRIPTPATH}/../px4_roscon_25:/home/ubuntu/roscon-25-workshop_ws/src/ \
     --name=px4-roscon-25 \
+    -p 18570:18570/udp \
     px4/roscon-25-workshop bash
