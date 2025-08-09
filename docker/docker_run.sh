b50db52552951d1c0ce5534da243b15d5776bf04
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2025-08-09 13:27:55 +0100

    chore: auxiliary scripts can be run from any location
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,7 +1,10 @@
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 docker run -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
     --rm \
     -e DISPLAY=$DISPLAY \
-    -v $(pwd)/px4_roscon_25:/home/ubuntu/roscon-25-workshop_ws/src/ \
+    -v ${SCRIPTPATH}/../px4_roscon_25:/home/ubuntu/roscon-25-workshop_ws/src/ \
     --name=px4-roscon-25 \
     px4/roscon-25-workshop bash
--- docker/docker_run_nogui.sh
+++ docker/docker_run_nogui.sh
@@ -1,5 +1,8 @@
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 docker run -it \
     --rm \
-    -v $(pwd)/px4_roscon_25:/home/ubuntu/roscon-25-workshop_ws/src/ \
+    -v ${SCRIPTPATH}/../px4_roscon_25:/home/ubuntu/roscon-25-workshop_ws/src/ \
     --name=px4-roscon-25 \
     px4/roscon-25-workshop bash
