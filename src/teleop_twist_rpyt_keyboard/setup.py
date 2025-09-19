9f536a77f4ce7f258566d0fb6ae19a2ddcc6ba03
patrikpordi <pordipatrik@gmail.com>
2025-09-19 15:23:06 -0600

    Added cfg and launch file placeholders Fixed dependency issues for teleop and teleop_twist_rpyt_keyboard

--- src/teleop_twist_rpyt_keyboard/setup.py
+++ src/teleop_twist_rpyt_keyboard/setup.py
@@ -1,12 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'teleop_twist_rpyt_keyboard'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[],
-    py_modules=['teleop_twist_rpyt_keyboard'],
+    py_modules=[package_name.replace('-', '_')],  # use module name
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='patrik_ark',
@@ -15,7 +14,12 @@ setup(
     license='TODO: License declaration',
     entry_points={
         'console_scripts': [
-            'teleop_twist_rpyt_keyboard = teleop_twist_rpyt_keyboard:main'
+            'teleop_twist_rpyt_keyboard = teleop_twist_rpyt_keyboard:main',
         ],
     },
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
 )
