b218d7cb2818cf8cb7c9bc5c0c5a886ada94e9fc
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2025-09-20 17:34:03 +0100

    feat (offboard_mode): add launch argument to run the XRCEDDS agent
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- px4_roscon_25/offboard_demo/launch/offboard_demo.launch.py
+++ px4_roscon_25/offboard_demo/launch/offboard_demo.launch.py
@@ -1,6 +1,9 @@
 import os
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
@@ -10,7 +13,14 @@ def generate_launch_description():
     
     clock_bridge_config_file = os.path.join(pkg_share,"cfg","clock_bridge.yaml")
 
+    run_uxrcedds_agent_arg = DeclareLaunchArgument(
+        "run_uxrcedds_agent",
+        default_value="false",
+        description="Whether to run the MicroXRCEdds Agent",
+    )
+
     return LaunchDescription([
+        run_uxrcedds_agent_arg,
         Node(
             package="ros_gz_bridge",
             executable="parameter_bridge",
@@ -27,5 +37,10 @@ def generate_launch_description():
             parameters=[
                 {"use_sim_time": True}
             ]
+        ),
+        ExecuteProcess(
+            cmd=["MicroXRCEAgent", "udp4", "-p", "8888", "-v", "3"],
+            output="screen",
+            condition=IfCondition(LaunchConfiguration("run_uxrcedds_agent"))
         )
     ])
